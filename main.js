aae1a8079030a1a2a97775d1f7267f443669e659
AhmedSalah22998 <159345207+AhmedSalah22998@users.noreply.github.com>
2024-02-25 15:25:30 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,164 @@
+// Function to detect if the device is a mobile device
+function isMobileDevice() {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
+
+// Function to detect the operating system
+function detectOS() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  // iOS detection
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
+
+const app = detectOS();
+
+// Example usage
+if (isMobileDevice()) {
+  console.log("This is a mobile device.");
+  //   document.body.innerHTML = "This is a mobile device ......";
+  document.body.innerHTML = `${
+    app === "Android"
+      ? `
+      <a
+        href="https://apps.apple.com/eg/app/ithraa/id6471949412"
+        target="_blank"
+        class="hover:scale-105 transition-transform"
+      >
+        <img
+          src="./img/app-store-google.svg"
+          class="h-12 pr-4 bounce-top-icons"
+        />
+      </a>
+      `
+      : app === "iOS"
+      ? `
+      <a
+        href="https://play.google.com/store/apps/details?id=io.ithraa.ithraa_app&pli=1"
+        target="_blank"
+        class="hover:scale-105 transition-transform"
+      >
+        <img src="./img/app-store-apple.svg" class="h-12 bounce-top-icons" />
+      </a> `
+      : `<div class="h-screen">
+        <div class="w-full container mx-auto p-6">
+          <div class="w-full flex justify-between">
+            <img src="./img/ithraa_logo.svg" class="w-20 inline" />
+          </div>
+        </div>
+
+        <div class="w-full container px-6 flex md:justify-between flex-wrap flex-col md:flex-row">
+          <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+            <h1 class="mt-4 mb-8 text-3xl md:text-4xl text-purple-800 font-bold leading-tight text-center md:text-right slide-in-bottom-h1">
+              تطبيقات منصة إثراء متاحة الآن علي الأنظمة التالية
+            </h1>
+
+            <p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center fade-in">
+              تحميل تطبيقنا
+            </p>
+            <div class="flex w-full justify-center md:justify-start gap-x-5 pb-24 lg:pb-0 fade-in mt-2">
+              <a
+                href="https://apps.apple.com/eg/app/ithraa/id6471949412"
+                target="_blank"
+                class="hover:scale-105 transition-transform"
+              >
+                <img
+                  src="./img/app-store-google.svg"
+                  class="h-12 pr-4 bounce-top-icons"
+                />
+              </a>
+
+              <a
+                href="https://play.google.com/store/apps/details?id=io.ithraa.ithraa_app&pli=1"
+                target="_blank"
+                class="hover:scale-105 transition-transform"
+              >
+                <img
+                  src="./img/app-store-apple.svg"
+                  class="h-12 bounce-top-icons"
+                />
+              </a>
+            </div>
+          </div>
+
+          <div class="w-full xl:w-3/5 py-6 overflow-y-hidden text-left">
+            <img
+              class="w-5/6 mx-auto lg:mr-0 slide-in-bottom"
+              src="https://tailwindtoolbox.github.io/App-Landing-Page/devices.svg"
+            />
+          </div>
+        </div>
+      </div>
+    `
+  }`;
+  console.log("Operating System:", detectOS());
+} else {
+  document.body.innerHTML = `
+    <div class="h-screen">
+     <div class="w-full container mx-auto p-6">
+        <div class="w-full flex justify-between">
+          <img src="./img/ithraa_logo.svg" class="w-20 inline" />
+        </div>
+      </div>
+
+      <div class="w-full container px-6 flex md:justify-between flex-wrap flex-col md:flex-row">
+        <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+          <h1 class="mt-4 mb-8 text-3xl md:text-4xl text-purple-800 font-bold leading-tight text-center md:text-right slide-in-bottom-h1">
+            تطبيقات منصة إثراء متاحة الآن علي الأنظمة التالية
+          </h1>
+
+          <p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center fade-in">
+            تحميل تطبيقنا
+          </p>
+          <div class="flex w-full justify-center md:justify-start gap-x-5 pb-24 lg:pb-0 fade-in mt-2">
+            <a
+              href="https://apps.apple.com/eg/app/ithraa/id6471949412"
+              target="_blank"
+              class="hover:scale-105 transition-transform"
+            >
+              <img
+                src="./img/app-store-google.svg"
+                class="h-12 pr-4 bounce-top-icons"
+              />
+            </a>
+
+            <a
+              href="https://play.google.com/store/apps/details?id=io.ithraa.ithraa_app&pli=1"
+              target="_blank"
+              class="hover:scale-105 transition-transform"
+            >
+              <img
+                src="./img/app-store-apple.svg"
+                class="h-12 bounce-top-icons"
+              />
+            </a>
+          </div>
+        </div>
+
+        <div class="w-full xl:w-3/5 py-6 overflow-y-hidden text-left">
+          <img
+            class="w-5/6 mx-auto lg:mr-0 slide-in-bottom"
+            src="https://tailwindtoolbox.github.io/App-Landing-Page/devices.svg"
+          />
+        </div>
+      </div>
+    </div>
+  `;
+  console.log("This is not a mobile device.");
+}
